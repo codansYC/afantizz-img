316b505dae79c454b9f7f176d30ab48089e75a01
codansYC <yuanchao@chushi007.com>
2018-02-24 14:40:10 +0800

    -

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -30,7 +30,6 @@ class UploadController extends BaseController {
     }
 
     function handleFile($file,$folder) {
-        var_dump($file);
         // 判断支持的格式
         $fileTypes = array('jpg', 'jpeg', 'gif', 'png');
         $fileParts = pathinfo($file['name']);
