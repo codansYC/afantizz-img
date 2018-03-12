b7e3787953277aebc7461f6902cecdee83e14d2a
codansYC <yuanchao@chushi007.com>
2018-03-12 22:49:05 +0800

    -

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -50,6 +50,7 @@ class UploadController extends BaseController {
         $fileFragment = explode("/", $file['type']);
         $extension = $fileFragment[1];
         $imgRes = $this->compressImage($extension,$tempFile);
+        echo '---------';return;
         imagejpeg($imgRes,$targetFile);
         $middleImageRes = $this->thumb($targetFile,800,600);
         imagejpeg($middleImageRes,$middleFile);
